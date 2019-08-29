b2659d3644a3667bd6b3065a78cd8ecbbd20f5bd
Talox1 <43228294+Talox1@users.noreply.github.com>
2019-08-28 19:31:00 -0500

    list item assignment

--- src/app/components/assignment-br/assignment-br.component.ts
+++ src/app/components/assignment-br/assignment-br.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
+import { AdminService } from 'src/app/services/admin.service';
+import { CaseroService } from 'src/app/services/casero.service';
 @Component({
   selector: 'app-assignment-br',
   templateUrl: './assignment-br.component.html',
@@ -12,13 +13,40 @@ export class AssignmentBRComponent implements OnInit {
 
   restaurantes : any = [];
 
-  constructor() {
-
+  constructor(
+    public adminservice:AdminService,
+    public bussinesService:AdminService,
+  ) {
+    
    }
 
   ngOnInit() {
+    this.getAllBussiness();
+    this.getAllRestaurants();
   }
+  getAllBussiness(){
+    this.bussinesService.getRestaurants().subscribe(
+      response=>{
+        console.log(response);
+        
+      }
+    )
+  }
+  
+  getAllRestaurants(){
+    this.adminservice.getUsers().subscribe(
+      response=>{
+        console.log(response.length)
+        for(let i = 0; i<response.length; i++){
+          if(response[i].is_casero == true){
+            console.log(response[i]);
+            this.restaurantes += response[i];
+          }
+        }
 
+      }
+    )
+  }
   
 
 }
--- src/app/components/view-admin/view-admin.component.ts
+++ src/app/components/view-admin/view-admin.component.ts
@@ -15,31 +15,9 @@ export class ViewAdminComponent implements OnInit {
 
   ngOnInit() {
     //listarNegocios
-    this.getAllBussiness();
-    this.getAllRestaurants();
+    
     
   }
 
-  getAllBussiness(){
-    this.bussinesService.getRestaurants().subscribe(
-      response=>{
-        console.log(response);
-        
-      }
-    )
-  }
   
-  getAllRestaurants(){
-    this.adminservice.getUsers().subscribe(
-      response=>{
-        console.log(response.length)
-        for(let i = 0; i<response.length; i++){
-          if(response[i].is_casero == true){
-            console.log(response[i]);
-          }
-        }
-
-      }
-    )
-  }
 }
