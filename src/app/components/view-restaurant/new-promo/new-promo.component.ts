f2d5417bc0c3a3c1b6533d14dd02cf7e8cea7d81
Rocío Valedo Santiago <43235226+RocioValedoStgo@users.noreply.github.com>
2019-10-09 22:44:30 -0500

    create new promo-propietario

--- src/app/components/view-restaurant/new-promo/new-promo.component.ts
+++ src/app/components/view-restaurant/new-promo/new-promo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
+import { OwnerService } from 'src/app/services/owner.service';
 
 @Component({
   selector: 'app-new-promo',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPromoComponent implements OnInit {
 
-  constructor() { }
+  estados;
+  ciudades;
+  days;
+
+  registerForm: FormGroup;
+  houseservice = true;
+
+  constructor(public fb: FormBuilder, public ownerService: OwnerService) { 
+    this.estados=['CHIAPAS', 'DURANGO', 'CDMX', 'TABASCO', 'OAXACA', 'TAMAULIPAS', 'MONTERREY', 'PUEBLA', 'GUADALAJARA', 'MORELIA'];
+    this.ciudades=["Tuxtla", "DF", "VILLA HERMOSA", "OAXACA", "SALTILLO", "PUEBLA", "MICHOACAN"];
+    this.days =["Lunes","Martes","Miercoles","Juevez","Viernes","Sabado","Domingo"];
+  }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      restaurant_id:[localStorage.getItem('id_restaurant')],
+      name: ['', [Validators.required]],
+      discount: ['', [Validators.required]],
+      numberReports: ['0'],
+      likes: ['0'],
+      // houseservice: [Boolean , [Validators.required] ],
+      // estado: ['', [Validators.required] ],
+      // ciudad: ['', [Validators.required] ],
+      availableDay: ['', [Validators.required] ],
+      restriction: ['', [Validators.required] ],
+    });
   }
+
+  onUploadFinish(event) {
+    console.log(event);
+   }
+  setservice(){
+  console.log(this.registerForm.value)
+  }
+  registarPromo(){
+    console.log(this.registerForm.value);
+    this.ownerService.promotionsRegister(this.registerForm.value).subscribe(
+      response =>{
+        console.log(response);
+
+      }
+    )
+  }  
 
 }
