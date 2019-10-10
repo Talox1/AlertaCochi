d788e7553a93c239523fd9472d5b967cac9dca98
Talox1 <43228294+Talox1@users.noreply.github.com>
2019-10-09 22:40:45 -0500

    create promo owner

--- src/app/components/view-restaurant/list-promos/list-promos.component.ts
+++ src/app/components/view-restaurant/list-promos/list-promos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OwnerService }  from '../../../services/owner.service'
 @Component({
   selector: 'app-list-promos',
   templateUrl: './list-promos.component.html',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPromosComponent implements OnInit {
 
-  constructor() { }
+  promos: any [] = [
+    {id: 0, name: "promo001", addres:"address001", service:true, month:"enero", date:'10', day:'lunes'},
+    {id: 2, name: "promo002", addres:"address002", service:true, month:"enero", date:'11', day:'martes'},
+    {id: 3, name: "promo003", addres:"address003", service:true, month:"enero", date:'12', day:'miercoles'},
+    {id: 4, name: "promo004", addres:"address004", service:true, month:"enero", date:'13', day:'jueves'},
+    {id: 5, name: "promo005", addres:"address005", service:true, month:"enero", date:'10,11,12', day:'lunes,martes,miercoles'}, 
+];
+  constructor(private ownerService: OwnerService) { }
 
   ngOnInit() {
+    this.ownerService.getPromotionsId(localStorage.getItem('id_restaurant')).subscribe(
+      response =>{
+      
+        this.promos = response;
+      
+      }
+    )
   }
 
+
+  editar(){
+
+  }
+
+  eliminar(){
+
+  }
 }
