b8f67c6216d21cbe802e56df2622809d524b3285
Talox1 <43228294+Talox1@users.noreply.github.com>
2019-11-18 13:30:58 -0600

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 const express = require ('express');
 const path = require ('ruta');
 
-aplicación const = express ();
+const app = express ();
 
 // Servir solo los archivos estáticos del directorio dist
-app.use (express.static (__ dirname + '/ dist / AlertaCochi'));
+app.use (express.static (__dirname + '/ dist / AlertaCochi'));
 
 app.get ('/ *', function (req, res) {
     
-res.sendFile (path.join (__ dirname + '/ dist / AlertaCochi /index.html'));
+res.sendFile (path.join (__dirname + '/ dist / AlertaCochi /index.html'));
 });
 
 // Inicie la aplicación escuchando en el puerto predeterminado de Heroku
