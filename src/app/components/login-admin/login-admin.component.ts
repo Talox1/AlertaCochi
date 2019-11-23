cd94e7b41c1b1fd925cc6be16cc3a8b94c16feab
Talox1 <43228294+Talox1@users.noreply.github.com>
2019-11-23 14:07:53 -0600

    login bug fixed

--- src/app/components/login-admin/login-admin.component.ts
+++ src/app/components/login-admin/login-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter, HostListener, OnInit, Injectable } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CanActivate, Router } from '@angular/router';//can activate para valida token
 
 
@@ -15,41 +15,44 @@ import { NavbarComponent } from '../navbar/navbar.component'
   styleUrls: ['./login-admin.component.css']
 })
 export class LoginAdminComponent implements OnInit {
-  email:string;
-  password:string;
+  email: string;
+  password: string;
 
   loginForm: FormGroup;
 
   constructor(
     public fb: FormBuilder,
-    private router: Router, 
+    private router: Router,
     public loginService: LoginService,
     public navbarService: NavbarService) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required] ],
-   });
+      password: ['', [Validators.required]],
+    });
   }
 
- 
 
-  login(){
+
+  login() {
     this.loginService.login(this.loginForm.value).subscribe(
       // tslint:disable-next-line: no-unused-expression
       response => {
-        
-        localStorage.setItem('token', response.token);
-        this.router.navigate(['/homeAdmin']);
-        localStorage.setItem('usuario','admin');
-        this.navbarService.toggle('admin');
-        
+        if (response.is_admin) {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/homeAdmin']);
+          localStorage.setItem('usuario', 'admin');
+          this.navbarService.toggle('admin');
+        } else {
+          document.getElementById('error').innerHTML = 'Correo o contraseña inválidos';
+          document.getElementById('error').className = 'notification is-danger';
+        }
+
+
       },
       error => {
-        console.log('status:' + error.status);
-        if (error.status == 401){
-          //alert("Correo o contraseña inválidos");
+        if (error.status == 401) {
           document.getElementById('error').innerHTML = 'Correo o contraseña inválidos';
           document.getElementById('error').className = 'notification is-danger';
         }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription, from } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 // import { renderComponent } from '@angular/core/src/render3';
@@ -17,24 +17,24 @@ import { NavbarComponent } from '../navbar/navbar.component'
 })
 export class LoginComponent implements OnInit {
 
-  email:string;
-  password:string;
+  email: string;
+  password: string;
   id_owner;
   loginForm: FormGroup;
 
   constructor(
-    public fb: FormBuilder,  
+    public fb: FormBuilder,
     public loginService: LoginService,
     private router: Router,
     public navbarService: NavbarService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required] ],
-   });
+      password: ['', [Validators.required]],
+    });
   }
 
   ngOnInit() {
-    
+
 
   }
 
@@ -43,18 +43,23 @@ export class LoginComponent implements OnInit {
       // tslint:disable-next-line: no-unused-expression
 
       response => {
-        console.log(response)
-        localStorage.setItem('token', response.token);
-        localStorage.setItem('id_owner',response.id);
-        localStorage.setItem('usuario','owner');
-        this.navbarService.toggle('owner');//cambia el valor booleano para que el navbar se renderize
-        this.router.navigate(['/homeRestaurant']);
-        
+        if (response.is_owner) {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('id_owner',response.id);
+          localStorage.setItem('usuario','owner');
+          this.navbarService.toggle('owner');//cambia el valor booleano para que el navbar se renderize
+          this.router.navigate(['/homeRestaurant']);
+        }else{
+          document.getElementById('error').innerHTML = 'Correo o contraseña inválidos';
+          document.getElementById('error').className = 'notification is-danger';
+        }
+
+
       },
       error => {
         console.log('status:' + error.status);
-        if (error.status == 401){
-          
+        if (error.status == 401) {
+
           document.getElementById('error').innerHTML = 'Correo o contraseña inválidos';
           document.getElementById('error').className = 'notification is-danger';
         }
